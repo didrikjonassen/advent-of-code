18ced4efbc72a5b4fb52bd40fe69b0c5f3b7eafd
Didrik Jonassen <jonassen.didrik@gmail.com>
2017-12-17 04:10:42 +0100

    Completed day 3

---
+++ src/3/SpiralMemoryEasy.kt
@@ -0,0 +1,27 @@
+package `3`
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val input = Integer.parseInt(File ("src/3/SpiralMemoryInput.txt").readText())
+    var layer = 1
+    var bottomRight = 9
+    while (bottomRight < input) {
+        layer++
+        bottomRight += 4*2*layer
+    }
+    val bottomLeft = bottomRight - 2*layer
+    val topLeft = bottomLeft - 2*layer
+    val topRight = topLeft - 2*layer
+    val distance = when {
+        input >= bottomLeft -> distanceFromCenter(bottomLeft, bottomRight, input)
+        input >= topLeft -> distanceFromCenter(topLeft, bottomLeft, input)
+        input >= topRight -> distanceFromCenter(topRight, topLeft, input)
+        else -> distanceFromCenter(topRight - 2*layer, topRight, input)
+    }
+    println("Distance = ${layer + distance}")
+
+}
+
+fun distanceFromCenter(start: Int, stop: Int, value: Int) =
+        Math.abs(value - (start+stop)/2)
---
+++ src/3/SpiralMemoryHard.kt
@@ -0,0 +1,51 @@
+package `3`
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val input = Integer.parseInt(File("src/3/SpiralMemoryInput.txt").readText())
+    val n = 11
+    val array = Array(n, { IntArray(n) })
+    val mid = n / 2
+
+    array[mid][mid] = 1
+    var layer = 1
+
+    while (true) {
+        (mid - layer + 1..mid + layer)
+                .forEach { i ->
+                    array[i][mid + layer] = array[i - 1][mid + layer] + (i - 1..i + 1).map { array[it][mid + layer - 1] }.sum()
+                    if (array[i][mid + layer] > input) {
+                        println(array[i][mid + layer])
+                        System.exit(0)
+                    }
+                }
+        (mid + layer - 1 downTo mid - layer)
+                .forEach { i ->
+                    array[mid + layer][i] = array[mid + layer][i + 1] + (i + 1 downTo i - 1).map { array[mid + layer - 1][it] }.sum()
+                    if (array[mid + layer][i] > input) {
+                        println(array[mid + layer][i])
+                        System.exit(0)
+                    }
+                }
+        (mid + layer - 1 downTo mid - layer)
+                .forEach { i ->
+                    array[i][mid - layer] = array[i + 1][mid - layer] + (i + 1 downTo i - 1).map { array[it][mid - layer + 1] }.sum()
+                    if (array[i][mid - layer] > input) {
+                        println(array[i][mid - layer])
+                        System.exit(0)
+                    }
+                }
+        (mid - layer + 1..mid + layer)
+                .forEach { i ->
+                    array[mid - layer][i] = array[mid - layer][i - 1] + (i - 1..i + 1).map { array[mid - layer + 1][it] }.sum()
+                    if (array[mid - layer][i] > input) {
+                        println(array[mid - layer][i])
+                        System.exit(0)
+                    }
+                }
+        layer++
+    }
+
+}
+
