fea3330ccfbcb1bf8bd91d3f24c54fa7cd0dd8b8
Didrik Jonassen <jonassen.didrik@gmail.com>
2017-12-17 05:04:52 +0100

    Completed day 5

---
+++ src/day5/AMazeOfTwistyTrampolinesAllAlikeHard.kt
@@ -0,0 +1,18 @@
+package day5
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val offsets = File("src/day5/AMazeOfTwistyTrampolinesAllAlikeInput.txt").readLines().map { Integer.parseInt(it) }.toMutableList()
+    var count = 0
+    try {
+        var pos = 0
+        while (true) {
+            val offset = if (offsets[pos] >= 3) offsets[pos]-- else offsets[pos]++
+            count++
+            pos += offset
+        }
+    } catch (e: IndexOutOfBoundsException) {
+        println("count = $count")
+    }
+}
