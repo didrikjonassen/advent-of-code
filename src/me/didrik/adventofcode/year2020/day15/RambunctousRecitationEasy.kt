c4ae99022baac23b223a51e167a6674bc83923a3
Didrik Jonassen <jonassen.didrik@gmail.com>
2020-12-15 06:46:43 +0100

    Completed day 15, 2020.

---
+++ src/me/didrik/adventofcode/year2020/day15/RambunctousRecitationEasy.kt
@@ -0,0 +1,22 @@
+package me.didrik.adventofcode.year2020.day15
+
+import java.io.File
+
+fun main() {
+    val file = File("src/me/didrik/adventofcode/year2020/day15/RambunctousRecitationInput.txt")
+
+    val input = file.readLines()[0].split(",").map { it.toInt() }
+    val spoken = input.mapIndexed {index, number -> number to (index + 1) }.dropLast(1).toMap().toMutableMap()
+    var previousNumber = input.last()
+    for (i in (input.size) until 2020) {
+        if (previousNumber !in spoken) {
+            spoken[previousNumber] = i
+            previousNumber = 0
+        } else {
+            val temp = previousNumber
+            previousNumber = i - spoken[previousNumber]!!
+            spoken[temp] = i
+        }
+    }
+    println(previousNumber)
+}
