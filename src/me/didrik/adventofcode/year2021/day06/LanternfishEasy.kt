39f11c320adf1de5f08050310a528b59c77dc6bc
Didrik Jonassen <jonassen.didrik@gmail.com>
2021-12-06 07:21:28 +0100

    Completed day 06, 2021.

---
+++ src/me/didrik/adventofcode/year2021/day06/LanternfishEasy.kt
@@ -0,0 +1,24 @@
+package me.didrik.adventofcode.year2021.day06
+
+import me.didrik.adventofcode.solve
+import java.io.File
+
+fun main() {
+    val file = File("src/me/didrik/adventofcode/year2021/day06/Input.txt")
+    val input = file.readLines().first().split(",").map { it.toInt() }
+    val days = 80
+    val timer = IntArray(9)
+    for (i in input) {
+        timer[i]++
+    }
+
+    repeat(days) {
+        val temp = timer[0]
+        for (i in 0 .. 7) {
+            timer[i] = timer[i + 1]
+        }
+        timer[8] = temp
+        timer[6] += temp
+    }
+    solve(timer.sum())
+}
