32e9d86b6ccce2a3d48f846e620f56af7a63244c
Didrik Jonassen <jonassen.didrik@gmail.com>
2021-12-07 06:52:34 +0100

    Completed day 07, 2021.

---
+++ src/me/didrik/adventofcode/year2021/day07/TheTreacheryOfWhalesHard.kt
@@ -0,0 +1,17 @@
+package me.didrik.adventofcode.year2021.day07
+
+import me.didrik.adventofcode.solve
+import java.io.File
+import kotlin.math.absoluteValue
+
+fun main() {
+    val file = File("src/me/didrik/adventofcode/year2021/day07/Input.txt")
+    val input = file.readLines().first().split(",").map { it.toInt() }
+
+    val min = (0..input.maxOrNull()!!).minOfOrNull { i ->
+        input.sumOf { (it - i).absoluteValue.moveCost() }
+    }
+    solve(min)
+}
+
+private fun Int.moveCost() = this * (this + 1) / 2
