7b6bc414be434a4170259b10a14e765251fb1815
Didrik Jonassen <jonassen.didrik@gmail.com>
2021-12-10 07:38:04 +0100

    Completed day 10, 2021.

---
+++ src/me/didrik/adventofcode/year2021/day10/SyntaxScoringHard.kt
@@ -0,0 +1,53 @@
+package me.didrik.adventofcode.year2021.day10
+
+import me.didrik.adventofcode.solve
+import java.io.File
+
+fun main() {
+    val file = File("src/me/didrik/adventofcode/year2021/day10/Input.txt")
+    val input = file.readLines()
+    val incomplete = input.filter { it.corrupted() == 0L }.map { it.incomplete() }.sorted()
+    solve(incomplete[incomplete.size/2])
+}
+
+private val open = listOf('(', '[', '{', '<')
+private val close = listOf(')', ']', '}', '>')
+private val points = listOf(3L, 57L, 1197L, 25137L)
+
+private fun String.corrupted(): Long {
+    val chars = this.toMutableList()
+    loop@
+    while (chars.isNotEmpty()) {
+        for (i in 0 until chars.size - 1) {
+            if (chars[i] in open && chars[i + 1] in close) {
+                if (open.indexOf(chars[i]) == close.indexOf(chars[i + 1])) {
+                    chars.removeAt(i)
+                    chars.removeAt(i)
+                    continue@loop
+                } else {
+                    return points[close.indexOf(chars[i+1])]
+                }
+            }
+        }
+        return 0
+    }
+    return 0
+}
+
+private fun String.incomplete(): Long {
+    val chars = this.toMutableList()
+    loop@
+    while (chars.isNotEmpty()) {
+        for (i in 0 until chars.size - 1) {
+            if (chars[i] in open && chars[i + 1] in close) {
+                if (open.indexOf(chars[i]) == close.indexOf(chars[i + 1])) {
+                    chars.removeAt(i)
+                    chars.removeAt(i)
+                    continue@loop
+                }
+            }
+        }
+        break
+    }
+    return chars.reversed().fold(0L) { acc, c -> acc * 5L + open.indexOf(c) + 1 }
+}
