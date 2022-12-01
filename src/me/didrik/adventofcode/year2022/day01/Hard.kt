4e5c3df8f40aab91207ac6ab4bf607ae96a932b0
Didrik Jonassen <jonassen.didrik@gmail.com>
2022-12-01 22:03:14 +0100

    Completed day 01, 2022.

---
+++ src/me/didrik/adventofcode/year2022/day01/Hard.kt
@@ -0,0 +1,18 @@
+package me.didrik.adventofcode.year2022.day01
+
+import me.didrik.adventofcode.solve
+import java.io.File
+
+fun main() {
+    val file = File("src/me/didrik/adventofcode/year2022/day01/Input.txt")
+    val input = file.readLines()
+    val sums = mutableListOf(0)
+    input.forEach {
+        if (it.isEmpty()) {
+            sums += 0
+        } else {
+            sums[sums.size - 1] += it.toInt()
+        }
+    }
+    solve(sums.sortedDescending().take(3).sum())
+}
