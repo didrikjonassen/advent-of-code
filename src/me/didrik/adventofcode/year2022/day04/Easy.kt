e6181846f8cd56ee23acecb14e2c41c1b48fb990
Didrik Jonassen <jonassen.didrik@gmail.com>
2022-12-04 12:35:02 +0100

    Completed day 04, 2022.

---
+++ src/me/didrik/adventofcode/year2022/day04/Easy.kt
@@ -0,0 +1,21 @@
+package me.didrik.adventofcode.year2022.day04
+
+import me.didrik.adventofcode.solve
+import java.io.File
+
+fun main() {
+    val file = File("src/me/didrik/adventofcode/year2022/day04/Input.txt")
+    val input = file.readLines()
+    var counter = 0
+    input.forEach {
+        val (foo, bar) = it.split(",").map { cleaningRange(it) }
+        if (foo.all { it in bar } || bar.all { it in foo })
+            counter++
+    }
+    solve(counter)
+}
+
+private fun cleaningRange(s: String): IntRange {
+    val (start, stop) = s.split("-")
+    return IntRange(start.toInt(), stop.toInt())
+}
